c12d48ee5feb957db63470e08421a321e1567634
Desarrollador Backend | Python (Flask/FastAPI) | PHP (Laravel/CodeIgniter) | IA & Cloud <85085152+hamintonjair@users.noreply.github.com>
2026-06-27 15:15:19 -0500

    se acualizo capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.mibolsillo.app',
+  appId: 'com.hamintonjair.mibolsillo',
   appName: 'MiBolsillo',
   webDir: 'dist'
 };
